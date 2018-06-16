ed1f2f1748ad23a63897e69e57fa2fc8c8025413
gaabrielaagm <monserratgm>
2018-06-16 13:26:49 -0500

    cambios

--- checklogin.php
+++ checklogin.php
@@ -3,18 +3,18 @@ include 'conexion.php';
 //obtener datos
 //obtener datos 
 $id_usuario=$_POST["id_usuario"];
-//echo "Usuario es:  "; echo $usuario; echo "<br>";
+echo "Usuario es:  "; echo $usuario; echo "<br>";
 
 $clave=$_POST["clave"];
-//echo "Contraseña es: "; echo $pass; echo "<br>";
+echo "Contraseña es: "; echo $pass; echo "<br>";
 
 @session_start();
 
-$verificar_usuario = mysqli_query($conexion,"SELECT * FROM usuarios WHERE Id_usuario = '$id_usuario'" );
+$verificar_usuario = mysqli_query($conexion,"SELECT * FROM usuario WHERE Id_usuario = '$id_usuario'" );
 
 if (mysqli_num_rows($verificar_usuario)>0) {
 	$row=mysqli_fetch_array($verificar_usuario);
-	if($row["clave"]==$clave){
+	if($row["Clave"]==$clave){
 		$_SESSION["usuario"] = $id_usuario;
 		$_SESSION["pass"] = $clave;
 		echo '<script>
