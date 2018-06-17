61f23cb1c06f75ef89a5d24fdd7666d5da3b4b42
gaabrielaagm <monserratgm>
2018-06-17 02:04:35 -0500

    Cambios
    Que Elimine por Id o por Nombre

--- eliminar_libro.php
+++ eliminar_libro.php
@@ -1,10 +1,10 @@
 <?php
 include 'conexion.php';
 
-$campo_buscar=$_POST["campo_buscar"];
+$busqueda=$_POST["busqueda"];
 $dato_campo=$_POST["dato_campo"];
 
-if($busqueda == '0'){
+if($busqueda == 'id'){
 	$verificar_libro = mysqli_query($conexion, "SELECT * FROM libro WHERE Id_libro = '$dato_campo'");
 	if(mysqli_num_rows($verificar_libro)>0){
 		$libro_delete = "DELETE FROM libro WHERE Id_libro='$dato_campo'";
@@ -25,11 +25,31 @@ if($busqueda == '0'){
 			  </script>'; 
 	}
 }
-if($busqueda== '1'){
+if($busqueda== 'nombre'){
 	$verificar_libro = mysqli_query($conexion, "SELECT * FROM libro WHERE Nombre = '$dato_campo'");
+	if(mysqli_num_rows($verificar_libro)>0){
+		$libro_delete = "DELETE FROM libro WHERE Nombre='$dato_campo'";
+		$resultado=mysqli_query($conexion,$libro_delete); 
+		if (!$resultado) {
+			echo '<script>
+			  alert("Error al eliminar en la BD");
+			  </script>';
+		}else{
+			echo '<script>
+			  confirm("El libro se ha eliminado");
+			  </script>';		
+		}		
+	}else{
+		echo '<script>
+			  alert("El libro no existe");
+			  window.history.go(-1);
+			  </script>'; 
+	}
 }
+mysqli_close($conexion); 
 
-//$verificar_libro = mysqli_query($conexion, "SELECT * FROM libro WHERE Nombre = '$nombre'");
+/*
+$verificar_libro = mysqli_query($conexion, "SELECT * FROM libro WHERE Nombre = '$nombre'");
 
 if(mysqli_num_rows($verificar_libro)>0){
 	$libro_delete = "DELETE FROM libro WHERE Nombre='$nombre'";
@@ -50,5 +70,5 @@ if(mysqli_num_rows($verificar_libro)>0){
 		  </script>'; 
 }
 mysqli_close($conexion); 
-
+*/
 ?>
