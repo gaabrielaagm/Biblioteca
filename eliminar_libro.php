317305235a094f0d884ef88a817c1d49bca6318d
Juan Manuel <freakshield777@hotmail.com>
2018-06-16 20:49:48 -0500

    ya jala pero bloquea los 2 campos

---
+++ eliminar_libro.php
@@ -0,0 +1,91 @@
+<?php
+include 'conexion.php';
+
+$nombre=$_POST["nombre"];
+
+$temas=$_POST["temas"]; 
+
+$idLibro=$_POST["id_libro"];
+
+
+
+if(isset($_POST["temas"])){
+	switch ($temas){
+		case '0':
+
+		$verificar_libro = mysqli_query($conexion, "SELECT * FROM libro WHERE Id_libro = $idLibro");
+
+		if(mysqli_num_rows($verificar_libro)>0){
+			$libro_delete = "DELETE FROM libro WHERE Id_libro=$idLibro";
+
+			$resultado=mysqli_query($conexion,$libro_delete); 
+			if (!$resultado) {
+				echo '<script>
+				  alert("Error al eliminar en la BD");
+				  </script>';
+
+			}else{
+
+
+				echo '<script>
+				  confirm("El libro se ha eliminado");
+				  </script>';
+				
+			}	
+			
+		}else{
+			echo '<script>
+				  alert("El libro no existe");
+				  
+				  </script>'; 
+			
+		}
+		mysqli_close($conexion); 
+
+		break;
+
+		case '1':
+
+
+$verificar_libro = mysqli_query($conexion, "SELECT * FROM libro WHERE Nombre = '$nombre'");
+
+if(mysqli_num_rows($verificar_libro)>0){
+	$libro_delete = "DELETE FROM libro WHERE Nombre='$nombre'";
+
+	$resultado=mysqli_query($conexion,$libro_delete); 
+	if (!$resultado) {
+		echo '<script>
+		  alert("Error al eliminar en la BD");
+		  </script>';
+
+	}else{
+
+
+		echo '<script>
+		  confirm("El libro se ha eliminado");
+		  </script>';
+		
+	}	
+	
+}else{
+	echo '<script>
+		  alert("El libro no existe");
+		  window.history.go(-1);
+		  </script>'; 
+	
+}
+mysqli_close($conexion); 
+
+
+}
+
+
+
+}
+
+
+
+
+
+
+?>
